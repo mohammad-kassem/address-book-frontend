6fd4a3d33030473af0455523beb2e5f05bfa88fc
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-25 08:56:51 +0300

    feat: linking the remove contact API to teh frontend

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -32,6 +32,24 @@ function Contacts(){
           })
     }
 
+    function removeContact(id){
+        let token = localStorage.getItem("token");
+        axios({
+            method: "delete",
+            url: `http://localhost:3000/api/contacts/remove/?id=${id}`,
+            headers: {
+              "Content-type": "application/json",
+              "Authorization" : token
+            }
+          })
+          .then(function(response){
+            setContacts(contacts.filter((contact) => contact._id !== id));
+          })
+          .catch(function(error){
+            alert(error.response.data.message);
+          })
+    }
+
     return(
         <>
             <div className="row-container">
@@ -43,14 +61,14 @@ function Contacts(){
                <div className="delete"></div>
                <div className="settings"></div>
             </div>
-            {contacts.map((contact)=>{return(
+            {contacts.map((contact, index)=>{return(
                  <div className="row-container" id={contact._id}>
                 <div className="name">{contact.name}</div>
                 <div className="email">{contact.email}</div>
                 <div className="phone">{contact.phone}</div>
                 <div className="relationship">{contact.relationship}</div>
                 <div className="location">{contact.location}</div>
-                <div className="delete" ><FaTrash/></div>
+                <div className="delete" onClick={()=>{removeContact(contact._id)}}><FaTrash/></div>
                 <div className="settings" onClick={() => {navigate(`${contact._id}`);}}><BsGearFill/></div>
                 </div>
             )})}
