a3191e81d4dd5cc6643eef52bae6dbdff9a5fbb7
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-26 20:15:49 +0300

    refactor: fixing button sizing

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({showFilter, setShowFilter, contacts, setContacts}){
     return(
-      <button className={showFilter ? `btn red` : `btn`}
+      <button className={showFilter ? `btn btn-header red` : `btn btn-header`}
       onClick={()=>{ console.log(contacts); setContacts(contacts); setShowFilter(!showFilter);}}>
         {showFilter ? "Clear" : "Filter"}
       </button>  
