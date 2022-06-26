40f36608ec1d4fb5bfb26bfb6c9f40099d82b910
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-06-26 23:31:09 +0300

    fea: implementing filtering based on country

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,45 +2,26 @@ import React from "react";
 import { useState } from "react";
 
 
-function FilterBar({ contacts, setContacts }){
-    const [filters, setFilters] = useState({name:['', '', ''],email:['', '', ''], phone:['', '', ''], relationship:['', '', '']});
-    const classes =["name", "email", "phone", "relationship"];
-    console.log(filters);
+function FilterBar({ contacts, setContacts, city, setCity }){
+    const [filters, setFilters] = useState({name:['', '', ''],email:['', '', ''], phone:['', '', ''], relationship:['', '', ''], country:['', '', '']});
+    const classes =["name", "email", "phone", "relationship", "country"];
 
     function handleChange(e, i, j){
+        {
         const temp = Object.values(filters).map((filter, index)=>{
             if (i !== index){
                 return filter
             }
             else {filter[j] = e.target.value; return filter}
-        })
+        })        
         let temp2={};
         for (let i = 0; i < temp.length; ++i)
             temp2[classes[i]] = temp[i];
+        console.log(temp2);
         setFilters(temp2);
-        // function filterFunction(contact){
-        // //     // if (contact.name.startsWith(filters[0][0]) && contact.name.includes(filters[0][1])&& contact.name.endsWith(filters[0][2])
-        // //     // && contact.email.startsWith(filters[1][0]) && contact.email.includes(filters[1][1]) && contact.email.endsWith(filters[1][2])
-        // //     // && contact.phone.startsWith(filters[2][0]) && contact.phone.includes(filters[2][1]) && contact.phone.endsWith(filters[2][2])
-        // //     // && contact.relationship.startsWith(filters[3][0]) && contact.relationship.includes(filters[3][1]) && contact.relationship.endsWith(filters[3][2])) 
-        // //     // return true;
-        //     let i = 0;
-        //     Object.values(contact).forEach((v)=>{
-        //         console.log(v);
-        //         if (i == 1 ){
-        //             console.log(v, i, filters[i-1]);
-        //             if ((!v.includes(filters[i-1][1])) 
-        //             || (!v.endsWith(filters[i-1][2])))
-        //             i ++; 
-        //             return false;
-        //         }
-        //         i = i+1;
-        //     })
-        //     return true;
-
-        // }
-
-        // setContacts(contacts.filter(filterFunction));
+        }
+        
+        let k = 0;
         setContacts(contacts.filter((contact)=>{
             for (var key in filters) {
                 if (!contact[key].startsWith(filters[key][0]) || !contact[key].includes(filters[key][1]) || !contact[key].endsWith(filters[key][2]))
@@ -49,6 +30,7 @@ function FilterBar({ contacts, setContacts }){
               return true;
             }));
     }
+
 
     return(
         <>
@@ -63,7 +45,6 @@ function FilterBar({ contacts, setContacts }){
             <input type="text" value={filter[2]} onChange={(e)=>{handleChange(e, i, 2)}}></input>
             </div>
         )})}
-        <div className="location"></div>
         <div className="btn-div"></div>
         </div>
         </>
